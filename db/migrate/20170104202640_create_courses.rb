3c83f462c426edc902349790d892ca07055c5bd1
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-01-04 22:44:13 +0200

    Add model Course

---
+++ db/migrate/20170104202640_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.integer :season
+      t.text :description
+      t.datetime :start_time
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/courses.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :course do
+    name "MyString"
+    season 1
+    description "MyText"
+    start_time "2017-01-04 22:26:40"
+  end
+end

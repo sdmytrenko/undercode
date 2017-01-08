af26c31cfdda559eff1db1370ed4fe32ce523dbe
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-01-08 17:08:44 +0200

    Add user profile views [#6]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   root to: 'visitors#index'
   devise_for :users
+  resources :users, only: [:index, :show]
+
   resources :courses, only: [:index, :show ]
 end

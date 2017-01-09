c14a39f21b2d551f537b70d04bd4eee9a11a391c
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-01-09 17:50:28 +0200

    Add Lectures views for user

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
 
-  resources :courses, only: [:index, :show ]
+  resources :courses, only: [:index, :show ] do
+    resources :lectures, only: [:index, :show] #, shallow: true
+  end
 
   namespace :admin do
-    resources :courses #, except: [:index, :show]
+    resources :courses, except: [:index, :show]
   end
 end

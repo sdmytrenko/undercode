884f347a50efd0e427fb083f33ffcd084a00056d
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-01-11 14:20:45 +0200

    Admin panel: add users edit and deactivation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109131226) do
+ActiveRecord::Schema.define(version: 20170111115826) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170109131226) do
     t.datetime "updated_at",                              null: false
     t.string   "name"
     t.string   "role",                   default: "user"
+    t.boolean  "active",                 default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

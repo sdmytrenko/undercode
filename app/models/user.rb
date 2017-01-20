23c47c87d6a9cc06f749933018ea8724a4dd6993
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-01-20 16:34:20 +0200

    Omniauth-github: [DONE]

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,9 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
+      user.name = auth.info.name
       user.email = auth.info.email
+      #user.image = auth.info.image
       user.password = Devise.friendly_token[0,20]
     end
   end

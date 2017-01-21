97ca66ad60de53ec1097e6c89b39c76652947ddc
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-01-21 18:57:54 +0200

    CRUD for lectures

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,30 +1,36 @@
 class LecturesController < ApplicationController
 
-  before_action :find_lecture, only: [:edit, :update, :show, :index, :destroy]
+  before_action :find_course
 
   def index
     @lectures = Lecture.where("course_id = #{params[:course_id]}")
   end
 
   def new
-    @lecture = Lecture.new
+    @lecture = @course.lectures.new
   end
 
   def create
-    @lecture = Lecture.new(lecture_params)
-    @lecture.course_id = params[:course_id]
+    @lecture = @course.lectures.new(lecture_params)
+
     if @lecture.save
-      redirect_to course_lecture_path(@lecture.course_id)
+      redirect_to course_lecture_path(@course, @lecture)
     else
       render :new
     end
   end
 
   def edit
+    @lecture = Lecture.find(params[:id])
   end
 
   def update
-    
+    @lecture = Lecture.find(params[:id])
+    if @lecture.update(lecture_params)
+      redirect_to course_lectures_path, flash: {notice: 'Post successfuly updated'}
+    else
+      render :edit
+    end
   end
 
   def show
@@ -32,12 +38,11 @@ class LecturesController < ApplicationController
   end
 
   def destroy
-    
   end
 
   private
-    def find_lecture
-      @lecture = Lecture.find(params[:course_id])
+    def find_course
+      @course = Course.find(params[:course_id])
     end
 
     def lecture_params

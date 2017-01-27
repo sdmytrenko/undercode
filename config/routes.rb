346bd5dd9f564adf4238ffeb26e787820c1a7fc6
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-01-27 14:03:59 +0200

    CRUD for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
 
   root to: 'visitors#index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :users, only: [:index, :show]
+
+  resources :posts #do
+#      resources :comments, except: [:index, :show, :new], shallow: true
+#  end
 
   resources :courses do #, only: [:index, :show ] do
     resources :lectures #, shallow: true #, only: [:index, :show]

3c6609d0ab71477d86ee8a571e65afbf68f20208
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2017-02-07 14:58:31 +0200

    Add avatar: migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205172623) do
+ActiveRecord::Schema.define(version: 20170207125226) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20170205172623) do
     t.boolean  "active",                 default: true
     t.string   "provider"
     t.string   "uid"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
